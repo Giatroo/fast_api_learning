5368966229aad7c247c37a21ec63788fc4f4e63c
Lucas Paiolla <lucaspaiolla@gmail.com>
2024-09-15 20:28:14 -0300

    Aula 3 - CRUD

--- fast_duno/app.py
+++ fast_duno/app.py
@@ -1,11 +1,13 @@
 from http import HTTPStatus
 
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from fastapi.responses import HTMLResponse
 
-from fast_duno.schemas import Message
+from fast_duno.schemas import Message, UserDB, UserList, UserPublic, UserSchema
 
 app = FastAPI()
+
+database = []
 
 
 @app.get("/", status_code=HTTPStatus.OK, response_model=Message)
@@ -16,3 +18,44 @@ def read_root():
 @app.get("/page", status_code=HTTPStatus.OK, response_class=HTMLResponse)
 def read_page():
     return """This is a page."""
+
+
+@app.post("/users/", status_code=HTTPStatus.CREATED, response_model=UserPublic)
+def create_user(user: UserSchema):
+    user_with_id = UserDB(id=len(database) + 1, **user.model_dump())
+    database.append(user_with_id)
+    return user_with_id
+
+
+@app.get("/users/", status_code=HTTPStatus.OK, response_model=UserList)
+def read_users():
+    return {"users": database}
+
+
+@app.put(
+    "/users/{user_id}", status_code=HTTPStatus.OK, response_model=UserPublic
+)
+def update_user(user_id: int, user: UserSchema):
+    if user_id < 1 or user_id > len(database):
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND,
+            detail="User not found",
+        )
+
+    user_index = user_id - 1
+    database[user_index] = UserDB(id=user_id, **user.model_dump())
+    return database[user_index]
+
+
+@app.delete(
+    "/users/{user_id}", status_code=HTTPStatus.OK, response_model=Message
+)
+def delete_user(user_id: int):
+    if user_id < 1 or user_id > len(database):
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND,
+            detail="User not found",
+        )
+
+    del database[user_id - 1]
+    return {"message": "User deleted"}
--- tests/test_app.py
+++ tests/test_app.py
@@ -2,13 +2,8 @@ from http import HTTPStatus
 
 from fastapi.testclient import TestClient
 
-from fast_duno.app import app
 
-
-def test_read_root_returns_ok_and_hello_world():
-    # Arrange
-    client = TestClient(app)
-
+def test_read_root_returns_ok_and_hello_world(client: TestClient):
     # Act
     response = client.get("/")
 
@@ -17,13 +12,75 @@ def test_read_root_returns_ok_and_hello_world():
     assert response.json() == {"message": "Hello world!"}
 
 
-def test_read_page_returns_ok_and_html():
-    # Arrange
-    client = TestClient(app)
-
+def test_read_page_returns_ok_and_html(client: TestClient):
     # Act
     response = client.get("/page")
 
     # Assert
     assert response.status_code == HTTPStatus.OK
     assert response.text == "This is a page."
+
+
+def test_create_user(client: TestClient):
+    # Act
+    response = client.post(
+        "/users/",
+        json={
+            "username": "user",
+            "email": "test@test.com",
+            "password": "password",
+        },
+    )
+
+    # Assert
+    assert response.status_code == HTTPStatus.CREATED
+    assert response.json() == {
+        "id": 1,
+        "username": "user",
+        "email": "test@test.com",
+    }
+
+
+def test_read_users(client: TestClient):
+    # Act
+    response = client.get("/users/")
+
+    # Assert
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        "users": [
+            {
+                "id": 1,
+                "username": "user",
+                "email": "test@test.com",
+            }
+        ]
+    }
+
+
+def test_update_user(client: TestClient):
+    # Arrange
+    response = client.put(
+        "/users/1",
+        json={
+            "username": "testuser2",
+            "email": "test@test.com",
+            "password": "password",
+        },
+    )
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        "id": 1,
+        "username": "testuser2",
+        "email": "test@test.com",
+    }
+
+
+def test_delete_user(client: TestClient):
+    # Act
+    response = client.delete("/users/1")
+
+    # Assert
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {"message": "User deleted"}
