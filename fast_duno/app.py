2fb4a051b79e1e851c1db7d431cbfde3ea22802f
Lucas Paiolla <lucaspaiolla@gmail.com>
2024-09-15 16:12:13 -0300

    Aula 2

--- fast_duno/app.py
+++ fast_duno/app.py
@@ -1,8 +1,18 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+from fast_duno.schemas import Message
 
 app = FastAPI()
 
 
-@app.get("/")
+@app.get("/", status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {"message": "Hello world!"}
+
+
+@app.get("/page", status_code=HTTPStatus.OK, response_class=HTMLResponse)
+def read_page():
+    return """This is a page."""
--- tests/test_app.py
+++ tests/test_app.py
@@ -15,3 +15,15 @@ def test_read_root_returns_ok_and_hello_world():
     # Assert
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {"message": "Hello world!"}
+
+
+def test_read_page_returns_ok_and_html():
+    # Arrange
+    client = TestClient(app)
+
+    # Act
+    response = client.get("/page")
+
+    # Assert
+    assert response.status_code == HTTPStatus.OK
+    assert response.text == "This is a page."
